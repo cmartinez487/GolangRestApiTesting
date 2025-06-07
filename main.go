5c7e11e9846202bf38b56c737942c17aaa00b2a5
cmartinez487 <crafamartinez487@gmail.com>
2025-06-07 08:18:14 -0400

    Agregando crud de task

--- main.go
+++ main.go
@@ -19,11 +19,19 @@ func main() {
 
 	r.HandleFunc("/", routes.HomeHandler)
 
+	// User routes
 	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
 	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
 	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
 	r.HandleFunc("/users/{id}", routes.PutUserHandler).Methods("PUT")
 	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+
+	// Task routes
+	r.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	r.HandleFunc("/tasks", routes.PostTasksHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", routes.PutTasksHandler).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", routes.DeleteTasksHandler).Methods("DELETE")
 
 	http.ListenAndServe(":3000", r)
 }
--- models/Task.go
+++ models/Task.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Task struct {
 	gorm.Model
 
-	Title       string `json:"title" gorm:"not nulll; unique_index"`
-	Description string
-	Done        bool `json:"done" gorm:"default:false"`
-	UserID      uint
+	Title       string `json:"title" gorm:"not nulll; unique_index type:varchar(100)"`
+	Description string `json:"description"`
+	Done        bool   `json:"done" gorm:"default:false"`
+	UserID      uint   `json:"user_id"`
 }
